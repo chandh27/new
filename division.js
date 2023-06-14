598d4657b16f21ac82c88afab35a1a15f3f4d587
chandin4 <chandinianithagowda@gmail.com>
2023-06-14 15:29:18 +0530

    division code

---
+++ division.js
@@ -0,0 +1,6 @@
+function division() {
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result = a/b
+document.getElementById("result").value=result
+}
